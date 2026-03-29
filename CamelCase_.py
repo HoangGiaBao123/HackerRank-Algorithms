911ca7b2f164eaf61e59432f23fd1a1287d9f778
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-03-29 21:40:42 +0700

    Create CamelCase_.py
    Implement camelcase function to count uppercase letters.

---
+++ CamelCase_.py
@@ -0,0 +1,17 @@
+import math
+import os
+import sys
+
+def camelcase(s):
+    total = 1
+    for char in s:
+        if char.isupper():
+            total += 1
+    return total
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = camelcase(s)
+    fptr.write(str(result) + '\n')
+    fptr.close()
