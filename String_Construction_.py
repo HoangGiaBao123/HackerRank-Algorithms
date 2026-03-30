8949bad2272fa1440779eb8cc4c1ecfe141027ad
Gia Bảo <hoanggiabao131012vn@gmail.com>
2026-03-30 12:56:58 +0700

    Create String_Construction_.py
    Implement string construction function to count unique characters.

---
+++ String_Construction_.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+def stringConstruction(s):
+    return len(set(s))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    q = int(input().strip())
+    for q_itr in range(q):
+        s = input()
+        result = stringConstruction(s)
+        fptr.write(str(result) + '\n')
+    fptr.close()
